ed354a9ae0d09b9661f7b5a71917ab22e0d168d1
naka267 <141595924+naka267@users.noreply.github.com>
2024-08-27 22:48:29 +0900

    Add files via upload

---
+++ 岸本版/html/test.php
@@ -0,0 +1,12 @@
+<?php
+$db_user='xs449197_tester1';
+$db_password='testtest1';
+$pdo = new PDO('mysql:host=localhost;dbname=xs449197_test;charset=utf8',$db_user,$db_password);
+foreach ($pdo->query('select * from user_table') as $row) {
+    echo '<p>';
+    echo htmlspecialchars($row['id']),':';
+    echo $row['email'],':';
+    echo $row['pass'],':';
+    echo '</p>';
+}
+?>
